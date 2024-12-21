c3206e173ec1afa7ecd35dd711baa2e7b981ee86
mano-3-1-2002 <manovijay383@gmail.com>
2024-12-21 13:43:09 +0530

    Update testbench.sv

--- 08.Full_adder_verification/testbench.sv
+++ 08.Full_adder_verification/testbench.sv
@@ -1,11 +1,14 @@
+
+`include "interface.sv"
+`include "test.sv"
 module testbench;
   intf intff();
   test tst(intff);
   
-  full_adder fa(.a(intff a),
-                .b(intff b),
-                .c(intff c),
-                .sum(intff sum),
-                .carry(intff carry));
+  full_adder FA(.a(intff.a),
+                .b(intff.b),
+                .c(intff.c),
+                .sum(intff.sum),
+                .carry(intff.carry));
 endmodule
                 
