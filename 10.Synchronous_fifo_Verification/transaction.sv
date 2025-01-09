a76de83a060a9197570d38b6dea5bba0229b4757
mano-3-1-2002 <manovijay383@gmail.com>
2025-01-09 22:41:33 +0530

    Update transaction.sv

--- 10.Synchronous_fifo_Verification/transaction.sv
+++ 10.Synchronous_fifo_Verification/transaction.sv
@@ -1,12 +1,1 @@
-class transaction;
-  rand bit oper;
-  bit rd,wr;
-  bit full,empty;
-  bit[7:0] din;
-  reg[7:0] dout;
-  bit reset;
-  bit clk;
-  
-  constraint open_ctrl{oper dist{1:/50,0:/50};}
-endclass
-  
+
