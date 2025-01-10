f2858a7ea31a5b5481ed16e48fca35e7329b781d
mano-3-1-2002 <manovijay383@gmail.com>
2025-01-10 16:01:03 +0530

    Update generator.sv

--- 10.Synchronous_fifo_Verification/generator.sv
+++ 10.Synchronous_fifo_Verification/generator.sv
@@ -1,1 +1,47 @@
+`include "transaction.sv"
+class generator;
 
+  transaction tr;           // Transaction object to generate and send
+
+  mailbox mbx;              // Mailbox for communication
+
+  int count = 0;            // Number of transactions to generate
+
+  int i = 0;                // Iteration counter 
+
+  event next;               // Event to signal when to send the next transaction
+
+ event done;               // Event to convey completion of requested number of transactions
+
+   
+
+  function new(mailbox mbx);
+    this.mbx = mbx;
+  endfunction 
+
+ 
+
+  task run(); 
+    tr = new();
+
+
+    repeat (count) begin
+
+      assert (tr.randomize) else $error("Randomization failed");
+
+      i++;
+
+      mbx.put(tr);
+
+      $display("[GEN] : Oper : %0d iteration : %0d", tr.oper, i);
+
+      @(next);
+
+     -> done;
+      //#10;
+    end
+  endtask
+
+  
+
+endclass
